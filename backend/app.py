d1f068c433af0e5ab8ef400cace1cf7c07bed140
Shreyas Rao <109731624+Humbledot00@users.noreply.github.com>
2025-05-19 13:27:42 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -195,4 +195,6 @@ def download_all():
     return send_file(zip_path, as_attachment=True)
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
